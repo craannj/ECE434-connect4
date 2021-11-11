9511ee0e57c28a909823711b28aebed6f1428b22
Nathaniel Craan <craannj@rose-hulman.edu>
2021-11-10 21:59:14 -0500

    Connect 4 V1

---
+++ pinconfig.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#Configures the pins for Connect 4
+
+config-pin P8_33 eqep
+config-pin P8_35 eqep
+
+config-pin P9_17 i2c
+config-pin P9_18 i2c
+
+config-pin P9_19 i2c
+config-pin P9_20 i2c
