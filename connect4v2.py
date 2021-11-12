860365cc76abbd2f483d6d06787b91860584e4d6
Nathaniel Craan <craannj@rose-hulman.edu>
2021-11-12 04:16:31 +0000

    Version 2

---
+++ connect4v2.py
@@ -0,0 +1,285 @@
+#!/usr/bin/env python3
+
+from Adafruit_BBIO.Encoder import RotaryEncoder, eQEP2, eQEP1
+import Adafruit_BBIO.GPIO as GPIO
+import smbus
+import time
+
+bus = smbus.SMBus(2)
+bus2 = smbus.SMBus(1) 
+matrix = 0x70
+adxl = 0x53
+
+out = "P9_11"
+out2 = "P9_13"
+button1 = "P9_42"
+
+GPIO.setup(out, GPIO.OUT)
+GPIO.setup(out2, GPIO.OUT)
+GPIO.setup(button1, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+#Encoder
+horizontal = RotaryEncoder(eQEP1)
+horizontal.setAbsolute()
+horizontal.enable()
+
+#Start in top left corner of the matrix
+xpos = 15   #Horizontal starting point
+ypos = 1     #Vertical starting point
+xpos_old = 15
+ypos_old = 1
+
+player = 1  # 1=red  2=green
+
+screen = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+         0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
+         
+clear = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+         0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
+
+# Start oscillator (p10)
+bus.write_byte_data(matrix, 0x21, 0)
+# Disp on, blink off (p11)
+bus.write_byte_data(matrix, 0x81, 0)
+# Full brightness (p15)
+bus.write_byte_data(matrix, 0xe7, 0)
+
+bus.write_i2c_block_data(matrix, 0, screen)
+
+bus2.write_byte_data(adxl, 0x2c, 0x08)
+bus2.write_byte_data(adxl, 0x2d, 0x08)
+bus2.write_byte_data(adxl, 0x31, 0x08)
+
+def placeRedPiece(xpos, xpos_old, ypos, ypos_old):
+    places = 0
+    GPIO.output(out, GPIO.LOW)
+    total = screen[xpos] + screen[xpos-1] - 1
+    xpos_old = screen[xpos] - 1
+    print(total)
+    if(total == 0):
+        for i in range(7):
+            ypos = ypos << 1
+            screen[xpos] = 0x00
+            screen[xpos] = screen[xpos] | ypos
+            bus.write_i2c_block_data(matrix, 0, screen)
+            ypos_old = ypos
+            time.sleep(0.25)
+        #screen[xpos] = screen[xpos] + 128
+        #bus.write_i2c_block_data(matrix, 0, screen)
+    
+    elif(total == 128):
+        for i in range(6):
+            ypos = ypos << 1
+            screen[xpos] = 0x00
+            screen[xpos] = screen[xpos] | ypos
+            bus.write_i2c_block_data(matrix, 0, screen)
+            ypos_old = ypos
+            time.sleep(0.25)
+        screen[xpos] = xpos_old + 64
+        bus.write_i2c_block_data(matrix, 0, screen)
+        
+    elif(total == 192):
+        for i in range(5):
+            ypos = ypos << 1
+            screen[xpos] = 0x00
+            screen[xpos] = screen[xpos] | ypos
+            bus.write_i2c_block_data(matrix, 0, screen)
+            ypos_old = ypos
+            time.sleep(0.25)
+        screen[xpos] = xpos_old + 32
+        bus.write_i2c_block_data(matrix, 0, screen)
+        
+    elif(total == 224):
+        for i in range(4):
+            ypos = ypos << 1
+            screen[xpos] = 0x00
+            screen[xpos] = screen[xpos] | ypos
+            bus.write_i2c_block_data(matrix, 0, screen)
+            ypos_old = ypos
+            time.sleep(0.25)
+        screen[xpos] = xpos_old + 16
+        bus.write_i2c_block_data(matrix, 0, screen)
+        
+    elif(total == 240):
+        for i in range(3):
+            ypos = ypos << 1
+            screen[xpos] = 0x00
+            screen[xpos] = screen[xpos] | ypos
+            bus.write_i2c_block_data(matrix, 0, screen)
+            ypos_old = ypos
+            time.sleep(0.25)
+        screen[xpos] = xpos_old + 8
+        bus.write_i2c_block_data(matrix, 0, screen)
+
+    elif(total == 248):
+        for i in range(2):
+            ypos = ypos << 1
+            screen[xpos] = 0x00
+            screen[xpos] = screen[xpos] | ypos
+            bus.write_i2c_block_data(matrix, 0, screen)
+            ypos_old = ypos
+            time.sleep(0.25)
+        screen[xpos] = xpos_old + 4
+        bus.write_i2c_block_data(matrix, 0, screen)
+    print(screen)
+    return screen, ypos
+    
+def placeGreenPiece(xpos, xpos_old, ypos, ypos_old):
+    places = 0
+    total = screen[xpos] + screen[xpos+1] - 1
+    xpos_old = screen[xpos] - 1
+    print(total)
+    GPIO.output(out, GPIO.LOW)
+    if(total == 0):
+        for i in range(7):
+            ypos = ypos << 1
+            screen[xpos] = 0x00
+            screen[xpos] = screen[xpos] | ypos
+            bus.write_i2c_block_data(matrix, 0, screen)
+            ypos_old = ypos
+            time.sleep(0.25)
+        #screen[xpos] = screen[xpos] + 128
+        #bus.write_i2c_block_data(matrix, 0, screen)
+    
+    elif(total == 128):
+        for i in range(6):
+            ypos = ypos << 1
+            screen[xpos] = 0x00
+            screen[xpos] = screen[xpos] | ypos
+            bus.write_i2c_block_data(matrix, 0, screen)
+            ypos_old = ypos
+            time.sleep(0.25)
+        screen[xpos] = xpos_old + 64
+        bus.write_i2c_block_data(matrix, 0, screen)
+        
+    elif(total == 192):
+        for i in range(5):
+            ypos = ypos << 1
+            screen[xpos] = 0x00
+            screen[xpos] = screen[xpos] | ypos
+            bus.write_i2c_block_data(matrix, 0, screen)
+            ypos_old = ypos
+            time.sleep(0.25)
+        screen[xpos] = xpos_old + 32
+        bus.write_i2c_block_data(matrix, 0, screen)
+        
+    elif(total == 224):
+        for i in range(4):
+            ypos = ypos << 1
+            screen[xpos] = 0x00
+            screen[xpos] = screen[xpos] | ypos
+            bus.write_i2c_block_data(matrix, 0, screen)
+            ypos_old = ypos
+            time.sleep(0.25)
+        screen[xpos] = xpos_old + 16
+        bus.write_i2c_block_data(matrix, 0, screen)
+        
+    elif(total == 240):
+        for i in range(3):
+            ypos = ypos << 1
+            screen[xpos] = 0x00
+            screen[xpos] = screen[xpos] | ypos
+            bus.write_i2c_block_data(matrix, 0, screen)
+            ypos_old = ypos
+            time.sleep(0.25)
+        screen[xpos] = xpos_old + 8
+        bus.write_i2c_block_data(matrix, 0, screen)
+
+    elif(total == 248):
+        for i in range(2):
+            ypos = ypos << 1
+            screen[xpos] = 0x00
+            screen[xpos] = screen[xpos] | ypos
+            bus.write_i2c_block_data(matrix, 0, screen)
+            ypos_old = ypos
+            time.sleep(0.25)
+        screen[xpos] = xpos_old + 4
+        bus.write_i2c_block_data(matrix, 0, screen)
+    print(screen)
+    return screen, ypos
+
+
+while True:
+    accl1 = bus2.read_byte_data(adxl, 0x32)
+    accl2 = bus2.read_byte_data(adxl, 0x33)
+    accl3 = bus2.read_byte_data(adxl, 0x34)
+    accl4 = bus2.read_byte_data(adxl, 0x35)
+    
+    acclx = ((accl2&0x03)) + accl1
+    accly = ((accl4&0x03)) + accl3    
+    
+    
+    if(player == 1):
+        GPIO.output(out, GPIO.HIGH)
+        GPIO.output(out2, GPIO.LOW)
+        if(horizontal.position < 0):
+            if(xpos < 15):
+                xpos = xpos + 2
+        elif(horizontal.position > 0):
+            if(xpos > 1):
+                xpos = xpos - 2
+        horizontal.position = 0
+
+    #Update the matrix
+        if(screen[xpos_old] > 0):
+            screen[xpos_old] = screen[xpos_old] - 1
+            bus.write_i2c_block_data(matrix, 0, screen)
+        screen[xpos] = screen[xpos] | ypos
+        bus.write_i2c_block_data(matrix, 0, screen)
+        xpos_old = xpos
+    
+        if(GPIO.input(button1) == GPIO.HIGH and screen[xpos] < 253):
+            screen, ypos = placeRedPiece(xpos, xpos_old, ypos, ypos_old)
+            bus.write_i2c_block_data(matrix, 0, screen)
+            xpos = 14
+            xpos_old = 0
+            ypos = 1
+            player = 2
+            #print(screen)
+       
+        if(accly < 200 and accly > 100):
+            for i in range(16):
+                screen[i] = 0x00
+            bus.write_i2c_block_data(matrix, 0, screen)
+            player = 1
+            xpos = 15
+            xpos_old = 0
+            ypos = 1            
+        #if(accly < 230 and accly > 128):
+         #   bus.write_i2c_block_data(matrix, 0, clear)
+            
+    elif(player == 2):
+        GPIO.output(out, GPIO.LOW)
+        GPIO.output(out2, GPIO.HIGH)
+        if(horizontal.position < 0):
+            if(xpos < 14):
+                xpos = xpos + 2
+        elif(horizontal.position > 0):
+            if(xpos > 1):
+                xpos = xpos - 2
+        horizontal.position = 0        
+        
+        if(screen[xpos_old] > 0):
+            screen[xpos_old] = screen[xpos_old] - 1
+            bus.write_i2c_block_data(matrix, 0, screen)
+        screen[xpos] = screen[xpos] | ypos
+        bus.write_i2c_block_data(matrix, 0, screen)
+        xpos_old = xpos        
+        
+        if(GPIO.input(button1) == GPIO.HIGH and screen[xpos] < 253):
+            screen, ypos = placeGreenPiece(xpos, xpos_old, ypos, ypos_old)
+            bus.write_i2c_block_data(matrix, 0, screen)            
+            xpos = 15
+            xpos_old = 0
+            ypos = 1
+            player = 1
+        
+        if(accly < 200 and accly > 100):
+            for i in range(16):
+                screen[i] = 0x00
+            bus.write_i2c_block_data(matrix, 0, screen)
+            xpos = 15
+            xpos_old = 0
+            ypos = 1
+            player = 1
+    
+    time.sleep(0.25)
