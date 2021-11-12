4c0f5433a3033c54a16ac78e1578f4f606e7154c
Nathaniel Craan <craannj@rose-hulman.edu>
2021-11-12 03:22:53 +0000

    Working version of Connect 4

--- connect4v1.py
+++ connect4v1.py
@@ -44,10 +44,13 @@ bus.write_byte_data(matrix, 0xe7, 0)
 bus.write_i2c_block_data(matrix, 0, screen)
 
 
-def placeRedPiece(xpos, ypos, ypos_old):
+def placeRedPiece(xpos, xpos_old, ypos, ypos_old):
     places = 0
     GPIO.output(out, GPIO.LOW)
-    if(screen[xpos] == 1):
+    total = screen[xpos] + screen[xpos-1] - 1
+    xpos_old = screen[xpos] - 1
+    print(total)
+    if(total == 0):
         for i in range(7):
             ypos = ypos << 1
             screen[xpos] = 0x00
@@ -55,8 +58,10 @@ def placeRedPiece(xpos, ypos, ypos_old):
             bus.write_i2c_block_data(matrix, 0, screen)
             ypos_old = ypos
             time.sleep(0.25)
+        #screen[xpos] = screen[xpos] + 128
+        #bus.write_i2c_block_data(matrix, 0, screen)
     
-    elif(screen[xpos] == 129):
+    elif(total == 128):
         for i in range(6):
             ypos = ypos << 1
             screen[xpos] = 0x00
@@ -64,10 +69,10 @@ def placeRedPiece(xpos, ypos, ypos_old):
             bus.write_i2c_block_data(matrix, 0, screen)
             ypos_old = ypos
             time.sleep(0.25)
-        screen[xpos] = 192
+        screen[xpos] = xpos_old + 64
         bus.write_i2c_block_data(matrix, 0, screen)
         
-    elif(screen[xpos] == 193):
+    elif(total == 192):
         for i in range(5):
             ypos = ypos << 1
             screen[xpos] = 0x00
@@ -75,10 +80,10 @@ def placeRedPiece(xpos, ypos, ypos_old):
             bus.write_i2c_block_data(matrix, 0, screen)
             ypos_old = ypos
             time.sleep(0.25)
-        screen[xpos] = 224
+        screen[xpos] = xpos_old + 32
         bus.write_i2c_block_data(matrix, 0, screen)
         
-    elif(screen[xpos] == 225):
+    elif(total == 224):
         for i in range(4):
             ypos = ypos << 1
             screen[xpos] = 0x00
@@ -86,10 +91,10 @@ def placeRedPiece(xpos, ypos, ypos_old):
             bus.write_i2c_block_data(matrix, 0, screen)
             ypos_old = ypos
             time.sleep(0.25)
-        screen[xpos] = 240
+        screen[xpos] = xpos_old + 16
         bus.write_i2c_block_data(matrix, 0, screen)
         
-    elif(screen[xpos] == 241):
+    elif(total == 240):
         for i in range(3):
             ypos = ypos << 1
             screen[xpos] = 0x00
@@ -97,10 +102,10 @@ def placeRedPiece(xpos, ypos, ypos_old):
             bus.write_i2c_block_data(matrix, 0, screen)
             ypos_old = ypos
             time.sleep(0.25)
-        screen[xpos] = 248
+        screen[xpos] = xpos_old + 8
         bus.write_i2c_block_data(matrix, 0, screen)
 
-    elif(screen[xpos] == 249):
+    elif(total == 248):
         for i in range(2):
             ypos = ypos << 1
             screen[xpos] = 0x00
@@ -108,11 +113,83 @@ def placeRedPiece(xpos, ypos, ypos_old):
             bus.write_i2c_block_data(matrix, 0, screen)
             ypos_old = ypos
             time.sleep(0.25)
-        screen[xpos] = 252
-        bus.write_i2c_block_data(matrix, 0, screen)    
+        screen[xpos] = xpos_old + 4
+        bus.write_i2c_block_data(matrix, 0, screen)
+    print(screen)
     return screen, ypos
     
-def placeGreenPiece(xpos, ypos, ypos_old):
+def placeGreenPiece(xpos, xpos_old, ypos, ypos_old):
+    places = 0
+    total = screen[xpos] + screen[xpos+1] - 1
+    xpos_old = screen[xpos] - 1
+    print(total)
+    GPIO.output(out, GPIO.LOW)
+    if(total == 0):
+        for i in range(7):
+            ypos = ypos << 1
+            screen[xpos] = 0x00
+            screen[xpos] = screen[xpos] | ypos
+            bus.write_i2c_block_data(matrix, 0, screen)
+            ypos_old = ypos
+            time.sleep(0.25)
+        #screen[xpos] = screen[xpos] + 128
+        #bus.write_i2c_block_data(matrix, 0, screen)
+    
+    elif(total == 128):
+        for i in range(6):
+            ypos = ypos << 1
+            screen[xpos] = 0x00
+            screen[xpos] = screen[xpos] | ypos
+            bus.write_i2c_block_data(matrix, 0, screen)
+            ypos_old = ypos
+            time.sleep(0.25)
+        screen[xpos] = xpos_old + 64
+        bus.write_i2c_block_data(matrix, 0, screen)
+        
+    elif(total == 192):
+        for i in range(5):
+            ypos = ypos << 1
+            screen[xpos] = 0x00
+            screen[xpos] = screen[xpos] | ypos
+            bus.write_i2c_block_data(matrix, 0, screen)
+            ypos_old = ypos
+            time.sleep(0.25)
+        screen[xpos] = xpos_old + 32
+        bus.write_i2c_block_data(matrix, 0, screen)
+        
+    elif(total == 224):
+        for i in range(4):
+            ypos = ypos << 1
+            screen[xpos] = 0x00
+            screen[xpos] = screen[xpos] | ypos
+            bus.write_i2c_block_data(matrix, 0, screen)
+            ypos_old = ypos
+            time.sleep(0.25)
+        screen[xpos] = xpos_old + 16
+        bus.write_i2c_block_data(matrix, 0, screen)
+        
+    elif(total == 240):
+        for i in range(3):
+            ypos = ypos << 1
+            screen[xpos] = 0x00
+            screen[xpos] = screen[xpos] | ypos
+            bus.write_i2c_block_data(matrix, 0, screen)
+            ypos_old = ypos
+            time.sleep(0.25)
+        screen[xpos] = xpos_old + 8
+        bus.write_i2c_block_data(matrix, 0, screen)
+
+    elif(total == 248):
+        for i in range(2):
+            ypos = ypos << 1
+            screen[xpos] = 0x00
+            screen[xpos] = screen[xpos] | ypos
+            bus.write_i2c_block_data(matrix, 0, screen)
+            ypos_old = ypos
+            time.sleep(0.25)
+        screen[xpos] = xpos_old + 4
+        bus.write_i2c_block_data(matrix, 0, screen)
+    print(screen)
     return screen, ypos
 
 
@@ -137,20 +214,42 @@ while True:
         xpos_old = xpos
     
         if(GPIO.input(button1) == GPIO.HIGH and screen[xpos] < 253):
-            screen, ypos = placeRedPiece(xpos, ypos, ypos_old)
+            screen, ypos = placeRedPiece(xpos, xpos_old, ypos, ypos_old)
             bus.write_i2c_block_data(matrix, 0, screen)
-            xpos = 15
+            xpos = 14
             xpos_old = 0
             ypos = 1
             player = 2
             #print(screen)
+            
     elif(player == 2):
         GPIO.output(out, GPIO.LOW)
         GPIO.output(out2, GPIO.HIGH)
-        #print("Player 2 placeholder")
-        if(GPIO.input(button1) == GPIO.HIGH):
+        if(horizontal.position < 0):
+            if(xpos < 14):
+                xpos = xpos + 2
+        elif(horizontal.position > 0):
+            if(xpos > 1):
+                xpos = xpos - 2
+        horizontal.position = 0        
+        
+        if(screen[xpos_old] > 0):
+            screen[xpos_old] = screen[xpos_old] - 1
+            bus.write_i2c_block_data(matrix, 0, screen)
+        screen[xpos] = screen[xpos] | ypos
+        bus.write_i2c_block_data(matrix, 0, screen)
+        xpos_old = xpos        
+        
+        if(GPIO.input(button1) == GPIO.HIGH and screen[xpos] < 253):
+            screen, ypos = placeGreenPiece(xpos, xpos_old, ypos, ypos_old)
+            bus.write_i2c_block_data(matrix, 0, screen)            
+            xpos = 15
+            xpos_old = 0
+            ypos = 1
             player = 1
-            
-            
+    
+    
+    
+    #print(screen)
     time.sleep(0.25)
     
